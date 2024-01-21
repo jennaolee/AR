b39604158a6034715f6b0f00d511550b6486a437
Jenna Lee <jennaolee03@gmail.com>
2024-01-20 17:42:04 -0800

    updated CSS and fixed spacing of images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, navigate
 
-show_pane=True
+show_pane=False
 
 page="""
 # ringAR
@@ -8,37 +8,51 @@ page="""
 <|layout|columns = 1 1 1 1
 
 <|card card-bg|
-Inside the card
-<|{content}|image|content=ring.jpg|>
-{: .p1 .mb1 }
+<|{content}|image|content=sapphireRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
+{: .p0 .m0 }
 |>
 
 <|card card-bg|
-Inside the card
-<|{content}|image|content=ring.jpg|>
-{: .p1 .mb1 }
+<|{content}|image|content=twistedRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
+{: .p0 .m0 }
 |>
 
 <|card card-bg|
-Inside the card
-<|{content}|image|content=ring.jpg|>
-{: .p1 .mb1 }
+<|{content}|image|content=pinkRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
+{: .p0 .m0 }
 |>
 
 <|card card-bg|
-Inside the card
-<|{content}|image|content=ring.jpg|>
-{: .p1 .mb1 }
+<|{content}|image|content=doubleRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
+{: .p0 .m0 }
 |>
 
-<|d-flex|
-<|{show_pane}|pane|persistent|width=100px|
-Pane content
+<|d-inline|
+<|{show_pane}|pane|persistent|anchor=bottom|
+information appears here
 |>
-This button can be pressed to open the persistent pane:
-<|Open|button|on_action={lambda s: s.assign("show_pane", True)}|>
 |>
+|>
+
+
 
 """
+stylekit = {
+  "color_primary": "#FFFFFF",
+  "color_secondary": "#5DADE2",
+  "color_error": "#F8C471", 
+  "color_warning": "#BA4A00", 
+  "color_success": "#2E4053", 
+  "color_background_light": "#76448A", 
+  "color_background_dark": "#E74C3C", 
+  "color_paper_light": "#ABEBC6", 
+  "color_paper_dark": "#D7BDE2", 
+  "font_family": "#5F6A6A", 
+ 
 
-Gui(page=page).run()
+}
+# Gui.run(stylekit=stylekit)
+Gui(page=page).run(stylekit=stylekit)
+
+# {: .p2 .mb1 }
+# #
