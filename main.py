154d128fdf24e6643df31ce1e0ed5e0f6d827bda
Jenna Lee <jennaolee03@gmail.com>
2024-01-20 18:33:18 -0800

    updated spacing of buttons

--- main.py
+++ main.py
@@ -3,37 +3,57 @@ from taipy.gui import Gui, navigate
 show_pane=False
 
 page="""
-# ringAR
+# **ringAR**{: .color-success}
 
 <|layout|columns = 1 1 1 1
 
 <|card card-bg|
 <|{content}|image|content=sapphireRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
-{: .p0 .m0 }
+<|d-inline|
+<|Try This|button|class_name=success|>
+<|Try This|button|class_name=success|>
+{: .ml3 .mr3}
 |>
-<|Try This|button|>
+{: .p0 .m-auto }
+|>
+
 
 <|card card-bg|
 <|{content}|image|content=twistedRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
-{: .p0 .m0 }
+<|d-inline|
+<|Try This|button|class_name=success|>
+<|Try This|button|class_name=success|>
+{: .ml3 .mr3}
 |>
-<|Try This|button|>
+{: .p0 .m-auto }
+|>
+
 
 <|card card-bg|
 <|{content}|image|content=pinkRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
-{: .p0 .m0 }
+<|d-inline|
+<|Try This|button|class_name=success|>
+<|Try This|button|class_name=success|>
+{: .ml3 .mr3}
 |>
-<|Try This|button|>
+{: .p0 .m-auto }
+|>
+
 
 <|card card-bg|
 <|{content}|image|content=doubleRing.png|width=275px|height=275px|on_action={lambda s: s.assign("show_pane", True)}|>
-{: .p0 .m0 }
+<|d-inline|
+<|Try This|button|class_name=success|>
+<|Try This|button|class_name=success|>
+{: .ml3 .mr3}
 |>
-<|Try This|button|>
+{: .p0 .m-auto }
+|>
+
 
 <|d-inline|
 <|{show_pane}|pane|persistent|anchor=bottom|
-information appears here
+**INFO HERE**{: .color-success}
 |>
 |>
 |>
@@ -44,19 +64,12 @@ information appears here
 stylekit = {
   "color_primary": "#FFFFFF",
   "color_secondary": "#5DADE2",
-  "color_error": "#F8C471", 
+  "color_error": "##1C2833", 
   "color_warning": "#BA4A00", 
   "color_success": "#2E4053", 
-  "color_background_light": "#76448A", 
-  "color_background_dark": "#E74C3C", 
-  "color_paper_light": "#ABEBC6", 
-  "color_paper_dark": "#D7BDE2", 
-  "font_family": "#5F6A6A", 
- 
-
+  "color_background_light": "#D8EFF9", 
+  "color_background_dark": "#D8EFF9", 
+  "color_paper_light": "#FDFEFE", 
+  "color_paper_dark": "#F6FEFF" 
 }
-# Gui.run(stylekit=stylekit)
 Gui(page=page).run(stylekit=stylekit)
-
-# {: .p2 .mb1 }
-# #
